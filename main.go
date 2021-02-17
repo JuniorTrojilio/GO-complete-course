764e70f6cc49712afa7866f65aaee655ef8245b8
Junior Trojilio <osmar.trojilio@gmail.com>
2021-02-17 15:37:10 -0300

    feat: create main package

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"api/src/router"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Start API")
+	r := router.Generate()
+	log.Fatal(http.ListenAndServe(":5000", r))
+}
