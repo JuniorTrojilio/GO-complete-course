41543f7e56a09a07ea0924d8be09499f96882abb
Junior Trojilio <osmar.trojilio@gmail.com>
2021-02-17 15:38:09 -0300

    feat: create routes to users

---
+++ src/router/routes/users.go
@@ -0,0 +1,40 @@
+package routes
+
+import (
+	"api/src/controllers"
+	"net/http"
+)
+
+var userRoutes = []Routes{
+	{
+		URI:         "/users",
+		Method:      http.MethodPost,
+		Handler:     controllers.CreateUser,
+		RequireAuth: false,
+	},
+	{
+		URI:         "/users",
+		Method:      http.MethodGet,
+		Handler:     controllers.ListUsers,
+		RequireAuth: false,
+	},
+	{
+		URI:         "/users/{userId}",
+		Method:      http.MethodGet,
+		Handler:     controllers.GetUser,
+		RequireAuth: false,
+	},
+	{
+		URI:         "/users/{userId}",
+		Method:      http.MethodPut,
+		Handler:     controllers.UpdateUser,
+		RequireAuth: false,
+	},
+
+	{
+		URI:         "/users/{userID}",
+		Method:      http.MethodDelete,
+		Handler:     controllers.DeleteUser,
+		RequireAuth: false,
+	},
+}
