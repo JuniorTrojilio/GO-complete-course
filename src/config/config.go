698f49b21dee8e917cc754b393ea16614d416a36
Junior Trojilio <osmar.trojilio@gmail.com>
2021-02-18 10:17:52 -0300

    feat: get data of .env

---
+++ src/config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/joho/godotenv"
+)
+
+var (
+	// ConnectionURI is a URI to connection on database
+	ConnectionURI = ""
+
+	//PORT define number to port where API runs
+	PORT = 0
+)
+
+// Load initializes environment variables
+func Load() {
+	var err error
+
+	if err = godotenv.Load(); err != nil {
+		log.Fatal(err)
+	}
+
+	PORT, err = strconv.Atoi(os.Getenv("PORT"))
+	if err != nil {
+		PORT = 9000
+	}
+
+	ConnectionURI = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+}
