c5cb8e7a041286984b822fc4ed2d6e1cfb2aad02
Junior Trojilio <osmar.trojilio@gmail.com>
2021-02-19 14:59:13 -0300

    feat: configure content type

--- src/presentation/json.go
+++ src/presentation/json.go
@@ -1,16 +1,26 @@
 package presentation
 
 import (
+	"api/src/models"
 	"encoding/json"
 	"log"
 	"net/http"
+	"strings"
 )
 
 // JSON converter received data in json response to request
 func JSON(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 
 	if err := json.NewEncoder(w).Encode(data); err != nil {
 		log.Fatal(err)
 	}
+}
+
+// RemoveBlankSpace removes blank spaces
+func RemoveBlankSpace(user *models.User) {
+	user.Name = strings.TrimSpace(user.Name)
+	user.Nick = strings.TrimSpace(user.Nick)
+	user.Email = strings.TrimSpace(user.Email)
 }
